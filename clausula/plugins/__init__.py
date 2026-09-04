51ec3cf1e20dcfe4ebc0f8c1f2de94f98fae29b6
Altair Li <137127498+Altairpaca@users.noreply.github.com>
2026-09-04 14:54:15 +0800

    feat: export plugin catalog and host policy contracts

--- clausula/plugins/__init__.py
+++ clausula/plugins/__init__.py
@@ -1,11 +1,24 @@
 """Controlled extension contracts for outer-layer plugins."""
 
-from .manifest import PluginManifest, PluginType
 from .bridge import CapabilityBridge, PluginPermissionError
+from .catalog import (
+    PLUGIN_ENTRY_POINT_GROUP,
+    PluginCatalog,
+    PluginCatalogError,
+    PluginPackage,
+)
+from .host_policy import PluginHostPolicy, PluginHostPolicyError
+from .manifest import PluginManifest, PluginType
 
 __all__ = [
     "CapabilityBridge",
+    "PLUGIN_ENTRY_POINT_GROUP",
+    "PluginCatalog",
+    "PluginCatalogError",
+    "PluginHostPolicy",
+    "PluginHostPolicyError",
     "PluginManifest",
+    "PluginPackage",
     "PluginPermissionError",
     "PluginType",
 ]
